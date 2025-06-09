4541321b7f9c3082ebf60195a950fc946fd7b55f
thiyaguRJ <thiyagutg02@gmail.com>
2025-06-09 22:08:39 +0530

    Keep Notes deployment changes

--- app/api/notes/[id]/route.tsx
+++ app/api/notes/[id]/route.tsx
@@ -4,24 +4,24 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(
   req: NextRequest,
-  params: { params: { id: string } }
+  context: { params: { id: string } }
 ) {
   await ConnectDB();
   const { title, content } = await req.json();
-  console.log(params.params.id, "params.id");
   const updated = await Notes.findByIdAndUpdate(
-    params.params.id,
+    context.params.id,
     { title, content },
     { new: true }
   );
   return NextResponse.json(updated);
 }
 
+
 export async function DELETE(
   req: NextRequest,
-  params: { params: { id: string } }
+  context: { params: { id: string } }
 ) {
   await ConnectDB();
-  await Notes.findByIdAndDelete(params.params.id);
+  await Notes.findByIdAndDelete(context.params.id);
   return NextResponse.json({ message: "Note deleted" });
 }
